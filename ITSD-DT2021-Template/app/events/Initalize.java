2e401721f4b74e7a59c9affbf8e43bb1e89eedd1
Chiara Pascucci <2522106P@student.gla.ac.uk>
2021-06-08 16:00:59 +0100

    improving avatar and board class - starting set up of movement logic for avatar (tested and the program runs and displays correctly but it needs a HumanPlayer and ComputerPlayer class to be create

--- ITSD-DT2021-Template/app/events/Initalize.java
+++ ITSD-DT2021-Template/app/events/Initalize.java
@@ -29,39 +29,44 @@ public class Initalize implements EventProcessor{
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		
 		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
-	//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
-		
 		//create board
-		Board board = new Board(out);
-		board.drawBoard();
-		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
-		
-		HumanPlayer p1 = new HumanPlayer();
-		ComputerPlayer p2 = new ComputerPlayer();
-		//create players (human and computer) --> player instantiation should trigger avatar instantiation 
-		//to test placing avatar on board here avatar are instantiated manually
-		
-		Avatar humanAvatar =  BasicObjectBuilders.loadAvatar(StaticConfFiles.humanAvatar, 0, Avatar.class);
-		humanAvatar.setOwner(p1, board);//assigning avatar to player and board - this could be done within player's class
-		
-		
-		Avatar computerAvatar = BasicObjectBuilders.loadAvatar(StaticConfFiles.aiAvatar, 1, Avatar.class);
-		computerAvatar.setOwner(p2, board);
-		
-		
-		//display avatars on board
-		Tile tOne = board.getTile(3, 2);
-		Tile tTwo = board.getTile(3, 8);
-		
-		BasicCommands.drawUnit(out, humanAvatar, tOne);
-		
-		BasicCommands.drawUnit(out, computerAvatar, tTwo);
-		
-		BasicCommands.setUnitAttack(out, humanAvatar, 2);
-		BasicCommands.setUnitHealth(out, humanAvatar, p1.getHealth());
-		
-		BasicCommands.setUnitAttack(out, computerAvatar, 2);
-		BasicCommands.setUnitHealth(out, computerAvatar, p2.getHealth());
+				Board board = new Board(out);
+				board.drawBoard();
+				//try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				HumanPlayer p1 = new HumanPlayer();
+				ComputerPlayer p2 = new ComputerPlayer();
+				//create players (human and computer) --> player instantiation should trigger avatar instantiation 
+				//to test placing avatar on board here avatar are instantiated manually
+				
+				Avatar humanAvatar =  (Avatar) BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 0, Avatar.class);
+				humanAvatar.setOwner(p1, board);//assigning avatar to player and board - this could be done within player's class
+				
+				
+				Avatar computerAvatar = (Avatar) BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 1, Avatar.class);
+				computerAvatar.setOwner(p2, board);
+				
+				
+				//display avatars on board
+				Tile tOne = board.getTile(2, 1);
+				Tile tTwo = board.getTile(2, 7);
+				
+				BasicCommands.drawUnit(out, humanAvatar, tOne);
+				try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				BasicCommands.drawUnit(out, computerAvatar, tTwo);	
+				try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				humanAvatar.setAttack(2);
+				computerAvatar.setAttack(2);
+				
+				BasicCommands.setUnitAttack(out, humanAvatar, humanAvatar.getAttack());
+				BasicCommands.setUnitHealth(out, humanAvatar, humanAvatar.getHealth());
+				try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				
+				BasicCommands.setUnitAttack(out, computerAvatar, computerAvatar.getAttack());
+				BasicCommands.setUnitHealth(out, computerAvatar, computerAvatar.getHealth());
 	}
 
 }
--- ITSD-DT2021-Template/app/structures/basic/Board.java
+++ ITSD-DT2021-Template/app/structures/basic/Board.java
@@ -6,26 +6,26 @@ import utils.BasicObjectBuilders;
 
 public class Board {
 	private Tile [][] gameBoard;
-	private final int width;
-	private final int heigth;
+	private final int Y;
+	private final int X;
 	private Tile humanStart;
 	private Tile computerStart;
 	private ActorRef out;
 	
 	public Board(ActorRef o) {
-		width = 9;
-		heigth = 5;
+		X = 9;
+		Y = 5;
 		this.out = o;
-		gameBoard = new Tile[heigth][width];
-		for (int i = 0; i<gameBoard.length; i++) {
-			for (int k = 0; k<gameBoard[0].length; k++) {
-				gameBoard [i][k] = BasicObjectBuilders.loadTile(i, k);
+		gameBoard = new Tile[Y][X];
+		for (int i = 0; i<Y; i++) {
+			for (int k = 0; k<X; k++) {
+				gameBoard [i][k] = BasicObjectBuilders.loadTile(k, i);
 						
 			}
 		}
 		
-		humanStart = gameBoard[3][2];
-		computerStart = gameBoard[3][8];
+		//humanStart = gameBoard[3][2];
+		//computerStart = gameBoard[3][8];
 	}
 
 	public Tile getHumanStart() {
@@ -37,8 +37,8 @@ public class Board {
 	}
 
 	public void drawBoard() {
-		for (int i = 0; i<heigth; i++) {
-			for (int k = 0; k<width; k++) {
+		for (int i = 0; i<Y; i++) {
+			for (int k = 0; k<X; k++) {
 				BasicCommands.drawTile(out, gameBoard [i][k],0);
 						
 			}
@@ -57,7 +57,6 @@ public class Board {
 	public Tile getTile(int h, int w) {
 		return gameBoard[h][w];
 	}
-	
 	
 
 }
