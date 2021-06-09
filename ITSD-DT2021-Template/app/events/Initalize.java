6f9f6a7ed3fb9bfc91c70e3e3dd101e38db54bf7
Nicholas Chui Wan Cheong <2082739c@student.gla.ac.uk>
2021-06-09 17:26:09 +0100

    Update Initalize.java

--- ITSD-DT2021-Template/app/events/Initalize.java
+++ ITSD-DT2021-Template/app/events/Initalize.java
@@ -3,15 +3,28 @@ package events;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
 import demo.CommandDemo;
 import structures.GameState;
+import structures.basic.Avatar;
+import structures.basic.Board;
+import structures.basic.ComputerPlayer;
+import structures.basic.HumanPlayer;
+import structures.basic.Tile;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
+
+/**
+ * Indicates that both the core game loop in the browser is starting, meaning
+ * that it is ready to recieve commands from the back-end.
+ * 
 
 /**
  * Indicates that both the core game loop in the browser is starting, meaning
  * that it is ready to recieve commands from the back-end.
  * 
  * { 
- *   messageType = “initalize”
+ *   messageType = initalize
  * }
  * 
  * @author Dr. Richard McCreadie
