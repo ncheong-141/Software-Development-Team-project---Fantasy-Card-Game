ffc70108102eb81ed007629af94329f72638232d
NoahS-Upton <noah.upton@gmail.com>
2021-06-10 13:11:49 +0100

    Deck initialisation methods, as well as card draw methods to hand completed

--- ITSD-DT2021-Template/app/structures/basic/Deck.java
+++ ITSD-DT2021-Template/app/structures/basic/Deck.java
@@ -1,5 +1,68 @@
 package structures.basic;
 
-public class Deck {
+import java.util.ArrayList;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
 
+public class Deck{
+	private ArrayList<Card> deck;
+	
+	public Deck() {
+		this.deck = null;
+	}
+	
+	private ArrayList<Card> deckOne() {
+		Card card;
+		String[] cardList= {
+				StaticConfFiles.c_azure_herald,
+				StaticConfFiles.c_azurite_lion,
+				StaticConfFiles.c_comodo_charger,
+				StaticConfFiles.c_fire_spitter,
+				StaticConfFiles.c_hailstone_golem,
+				StaticConfFiles.c_ironcliff_guardian,
+				StaticConfFiles.c_pureblade_enforcer,
+				StaticConfFiles.c_silverguard_knight,
+				StaticConfFiles.c_sundrop_elixir,
+				StaticConfFiles.c_truestrike};
+		for (int i=0; i<=9; i++) {
+			card = BasicObjectBuilders.loadCard(cardList[i], i+1, Card.class);
+			deck.add(card);
+			card = BasicObjectBuilders.loadCard(cardList[i], i+11, Card.class);
+			deck.add(card);
+			}
+		return deck;
+		}
+		
+	private ArrayList<Card> deckTwo() {
+		Card card;
+		String[] cardList= {
+				StaticConfFiles.c_blaze_hound,
+				StaticConfFiles.c_bloodshard_golem,
+				StaticConfFiles.c_entropic_decay,
+				StaticConfFiles.c_hailstone_golem,
+				StaticConfFiles.c_planar_scout,
+				StaticConfFiles.c_pyromancer,
+				StaticConfFiles.c_serpenti,
+				StaticConfFiles.c_rock_pulveriser,
+				StaticConfFiles.c_staff_of_ykir,
+				StaticConfFiles.c_windshrike,};
+		for (int i=0; i<=9; i++) {
+			card = BasicObjectBuilders.loadCard(cardList[i], i+1, Card.class);
+			deck.add(card);
+			card = BasicObjectBuilders.loadCard(cardList[i], i+11, Card.class);
+			deck.add(card);
+			}
+		return deck;
+		}
+		
+	public void delCard(int i){
+		deck.remove(i);
+	}
+	public void setDeck(ArrayList<Card> deck) {
+		this.deck = deck;
+	}
+	public ArrayList<Card> getDeck() {
+		return deck;
+	}
+	
 }
--- ITSD-DT2021-Template/app/structures/basic/Hand.java
+++ ITSD-DT2021-Template/app/structures/basic/Hand.java
@@ -1,5 +1,69 @@
 package structures.basic;
 
-public class Hand {
+import java.util.ArrayList;
 
+import akka.actor.ActorRef;
+import commands.BasicCommands;
+import structures.basic.Card;
+
+public class Hand {
+	private int curr;
+	private ArrayList hand;
+	
+	public Hand( ArrayList<Card> hand) {
+		super();
+		this.curr = 0;
+		this.hand = hand;
+	}
+	
+	public void initialHand(ActorRef out, Deck deck) {
+		ArrayList<Card> drawDeck= deck.getDeck();
+		Card cardOne= drawDeck.get(0);
+		Card cardTwo= drawDeck.get(1);
+		Card cardThree= drawDeck.get(2);
+		
+		BasicCommands.drawCard(out, cardOne, 1, 0);
+		BasicCommands.drawCard(out, cardTwo, 2, 0);
+		BasicCommands.drawCard(out, cardThree, 3, 0);
+		
+		hand.add(cardOne);
+		hand.add(cardTwo);
+		hand.add(cardThree);
+		 
+		deck.delCard(0);
+		deck.delCard(1);
+		deck.delCard(2);
+		
+		setCurr(3);
+	}
+	
+	public void drawCard(ActorRef out, Deck deck) {
+		curr=getCurr();
+		if (curr<6) {
+		ArrayList<Card> drawDeck= deck.getDeck();
+		Card drawn= drawDeck.get(0);
+		
+		curr++;
+		BasicCommands.drawCard(out, drawn, curr, 0);
+		
+		deck.delCard(0);
+		setCurr(curr);
+		}
+		else {
+			BasicCommands.addPlayer1Notification(out, "Hand Full", 2);
+		}
+	}
+	
+	public int getCurr() {
+		return curr;
+	}
+	public void setCurr(int curr) {
+		this.curr = curr;
+	}
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+	public void setHand(ArrayList<Card> hand) {
+		this.hand = hand;
+	}
 }
