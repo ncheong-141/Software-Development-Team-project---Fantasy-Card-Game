6568e0ca5ac7bc3646d155f1d9be9d95b57e34d8
Nicholas <nicholascheong141@gmail.com>
2021-06-11 18:45:08 +0200

    - Changed TileClicked event handler to support seperate implementation

--- ITSD-DT2021-Template/app/events/TileClicked.java
+++ ITSD-DT2021-Template/app/events/TileClicked.java
@@ -32,6 +32,63 @@ public class TileClicked implements EventProcessor{
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 
+		// Code user name
+		/*
+		 * 0 - Nicholas
+		 * 1 - Yufen
+		 * 2 - Victoria 
+		 * 3 - Chiara
+		 * 4 - Noah
+		 */
+		int user = 0;
+		
+		// Switch statement to change code user implementation
+		switch (user) {
+		
+		case (0):{
+			System.out.println("TileClicked user: Nicholas"); 
+			nicholasImpl(out, gameState, message);
+			break;
+		}
+		case (1):{
+			System.out.println("TileClicked user: Yufen");
+			yufenImpl(out, gameState, message);
+			break;
+		}
+		case (2):{
+			System.out.println("TileClicked user: Victoria");
+			victoriaImpl(out, gameState, message);
+			break;
+		}
+		case (3):{
+			System.out.println("TileClicked user: Chiara");
+			chiaraImpl(out, gameState, message);
+			break;
+		}
+		case (4):{
+			System.out.println("TileClicked user: Noah"); 
+			noahImpl(out, gameState, message);
+			break;
+		}
+		default: {
+			System.out.println("Please select user for TileClicked"); 
+		}
+		}
+
+	}
+
+
+	/* ======================================================================
+	 * User methods 
+	 * 
+	 * Call the method with your name and implement any code here (copy any other persons code when needed)
+	 * e.g. Copy code in chiaraImpl to click avatar or for monster you can use victorias or mine etc. 
+	 * This is just to avoid any merge conflicts. 
+	 * 
+	 * ====================================================================== */
+	
+	private void nicholasImpl(ActorRef out, GameState gameState, JsonNode message) {
+		
 		int tilex = message.get("tilex").asInt();
 		int tiley = message.get("tiley").asInt();
 		
@@ -51,17 +108,89 @@ public class TileClicked implements EventProcessor{
 			else {
 				System.out.println("Monster clicked");
 				Monster m = (Monster) gameState.getBoard().getTile(tiley, tilex).getUnitOnTile();
+				
+				// Do something like highlight unit etc. 
+				System.out.println("Monster clicked");
+				monsterLogic(m, gameState, out);
+
+			}
+		}
+	}
+	
+	private void yufenImpl(ActorRef out, GameState gameState, JsonNode message) {
+
+		int tilex = message.get("tilex").asInt();
+		int tiley = message.get("tiley").asInt();
+	}
+	
+	private void victoriaImpl(ActorRef out, GameState gameState, JsonNode message) {
+
+		int tilex = message.get("tilex").asInt();
+		int tiley = message.get("tiley").asInt();
+		
+		// Check if Unit present
+		if (gameState.getBoard().getTile(tiley , tilex).getUnitOnTile() != null) {
+			
+			System.out.println("Unit present");
+		
+			// Check if avatar  (i dont think we need to distinguish between avatar or monster here but for the sake of the print) 
+			if (gameState.getBoard().getTile(tiley, tilex).getUnitOnTile() instanceof Avatar) {
+				
+				System.out.println("Avatar clicked");
+				Avatar a = (Avatar) gameState.getBoard().getTile(tiley, tilex).getUnitOnTile();
+				avatarLogic(a, gameState, out);
+			}
+			else {
+				System.out.println("Monster clicked");
+				Monster m = (Monster) gameState.getBoard().getTile(tiley, tilex).getUnitOnTile();
+				
+				// Do something like highlight unit etc. 
+				System.out.println("Monster clicked");
 				monsterLogic(m, gameState, out);
 			}
 		}
-	
 	}
+	
+	private void chiaraImpl(ActorRef out, GameState gameState, JsonNode message) {
 
+		int tilex = message.get("tilex").asInt();
+		int tiley = message.get("tiley").asInt();
+		
+		// Check if Unit present
+		if (gameState.getBoard().getTile(tiley , tilex).getUnitOnTile() != null) {
+			
+			System.out.println("Unit present");
+		
+			// Check if avatar  (i dont think we need to distinguish between avatar or monster here but for the sake of the print) 
+			if (gameState.getBoard().getTile(tiley, tilex).getUnitOnTile() instanceof Avatar) {
+				
+				System.out.println("Avatar clicked");
+				Avatar a = (Avatar) gameState.getBoard().getTile(tiley, tilex).getUnitOnTile();
+				avatarLogic(a, gameState, out);
+			}
+			else {
+				System.out.println("Monster clicked");
+				Monster m = (Monster) gameState.getBoard().getTile(tiley, tilex).getUnitOnTile();
+				
+				// Do something like highlight unit etc. 
+				System.out.println("Monster clicked");
+				monsterLogic(m, gameState, out);
+			}
+		}
+	}
+	
+	private void noahImpl(ActorRef out, GameState gameState, JsonNode message) {
+
+		int tilex = message.get("tilex").asInt();
+		int tiley = message.get("tiley").asInt();
+	}
+	
+	
+	
 	/* Helper methods such as highlight unit, display unit stats etc */
 
 	static void monsterLogic(Monster m, GameState g, ActorRef o) {
-		// Do something like highlight unit etc. 
-		System.out.println("Monster clicked");
+
 		System.out.println("Monster name: " + m.getName());
 		System.out.println("Monster HP: " + m.getHP());
 		System.out.println("Monster attack: " + m.getAttackValue());
@@ -79,4 +208,6 @@ public class TileClicked implements EventProcessor{
 		}
 	}
 
+	
+	
 }
