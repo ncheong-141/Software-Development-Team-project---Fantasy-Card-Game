f8e329db8a116307a29475accf001388af17670a
NoahS-Upton <2190985u@student.gla.ac.uk>
2021-06-18 12:20:30 +0100

    added unit arrays and population to player and ai deck creation methods

--- ITSD-DT2021-Template/app/structures/basic/Deck.java
+++ ITSD-DT2021-Template/app/structures/basic/Deck.java
@@ -6,13 +6,16 @@ import utils.StaticConfFiles;
 
 public class Deck{//class used to create and manage player and ai decks
 	private ArrayList<Card> deck;// array of card objects that comprise the deck
+	private ArrayList<Unit> unitDeck;
 	
 	public Deck() { //constructor for deck
 		this.deck = new ArrayList<Card>();
+		this.unitDeck = new ArrayList<Unit>();
 	}
 	
 	public void deckOne() {// creates an instance of the human player deck
 		Card card;
+		Unit unit;
 		String[] cardList= {// list of cards in player deck
 				StaticConfFiles.c_azure_herald,
 				StaticConfFiles.c_azurite_lion,
@@ -24,35 +27,66 @@ public class Deck{//class used to create and manage player and ai decks
 				StaticConfFiles.c_silverguard_knight,
 				StaticConfFiles.c_sundrop_elixir,
 				StaticConfFiles.c_truestrike};
+		String[] unitList= {// list of units in player deck
+				StaticConfFiles.u_azure_herald,
+				StaticConfFiles.u_azurite_lion,
+				StaticConfFiles.u_comodo_charger,
+				StaticConfFiles.u_fire_spitter,
+				StaticConfFiles.u_hailstone_golem,
+				StaticConfFiles.u_ironcliff_guardian,
+				StaticConfFiles.u_pureblade_enforcer,
+				StaticConfFiles.u_silverguard_knight};
+		
+		
 		for (int i=0; i<=9; i++) {// cycles through the list and creates two instances of each card
 			card = BasicObjectBuilders.loadCard(cardList[i], i+1, Card.class);
 			deck.add(card);
 			card = BasicObjectBuilders.loadCard(cardList[i], i+11, Card.class);
 			deck.add(card);
 			}
-		
-		
+		for(int j=0; j<=7; j++)//cycles through unit list and creates two of each unit
+			unit= BasicObjectBuilders.loadUnit(unitList[j], j+1, Unit.class);
+			unitDeck.add(unit);
+			unit= BasicObjectBuilders.loadUnit(unitList[j], j+11, Unit.class);
+			unitDeck.add(unit);
 		}
 		
 	public void deckTwo() {// creates AI player deck
 		Card card;
+		Unit unit;
 		String[] cardList= {//list of cards in AI player deck
 				StaticConfFiles.c_blaze_hound,
 				StaticConfFiles.c_bloodshard_golem,
-				StaticConfFiles.c_entropic_decay,
 				StaticConfFiles.c_hailstone_golem,
 				StaticConfFiles.c_planar_scout,
 				StaticConfFiles.c_pyromancer,
+				StaticConfFiles.c_windshrike,
 				StaticConfFiles.c_serpenti,
 				StaticConfFiles.c_rock_pulveriser,
-				StaticConfFiles.c_staff_of_ykir,
-				StaticConfFiles.c_windshrike,};
+				StaticConfFiles.c_entropic_decay,
+				StaticConfFiles.c_staff_of_ykir};
+		String[] unitList= {//list of units in AI player deck
+				StaticConfFiles.u_blaze_hound,
+				StaticConfFiles.u_bloodshard_golem,
+				StaticConfFiles.u_hailstone_golem,
+				StaticConfFiles.u_planar_scout,
+				StaticConfFiles.u_pyromancer,
+				StaticConfFiles.u_windshrike,
+				StaticConfFiles.u_serpenti,
+				StaticConfFiles.u_rock_pulveriser};
+		
 		for (int i=0; i<=9; i++) {//cycles through card list twice and creates two of each card and adds to deck
 			card = BasicObjectBuilders.loadCard(cardList[i], i+1, Card.class);
 			deck.add(card);
 			card = BasicObjectBuilders.loadCard(cardList[i], i+11, Card.class);
 			deck.add(card);
 			}
+		for(int j=0; j<=7; j++)//cycles through unit list and creates two of each unit
+			unit= BasicObjectBuilders.loadUnit(unitList[j], j+1, Unit.class);
+			unitDeck.add(unit);
+			unit= BasicObjectBuilders.loadUnit(unitList[j], j+11, Unit.class);
+			unitDeck.add(unit);
+		}
 		}
 		
 	public void delCard(int i){// removes card from the deck
