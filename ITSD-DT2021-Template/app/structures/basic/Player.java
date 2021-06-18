7fbc209a56dc8ff158f2f0a4973a950fc521af3a
2502089c <2502089c@student.gla.ac.uk>
2021-06-18 11:04:20 +0100

    Player HumanPlayer with avatar and deck method update

--- ITSD-DT2021-Template/app/structures/basic/Player.java
+++ ITSD-DT2021-Template/app/structures/basic/Player.java
@@ -1,5 +1,14 @@
 package structures.basic;
-import structures.*;
+
+import commands.BasicCommands;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.JsonNode;
+import actors.GameActor;
+import akka.actor.ActorRef;
+import events.CardClicked;
+import events.EndTurnClicked;
+import structures.GameState;
 /**
  * A basic representation of of the Player. A player
  * has health and mana.
@@ -9,40 +18,33 @@ import structures.*;
  */
 public class Player {
 
-	int health;
-	int mana;
-	Avatar avatar;
-	Deck deck;
-	Hand hand;
-	boolean dead;
-	boolean turn;
+	private int health;
+	private int mana;
+	protected Avatar avatar;
+	protected Deck deck;
+	protected Hand hand;
+	protected ActorRef out;
+	private boolean dead;
+	private boolean turn;
 	
 	
 	public Player() {
 		super();
 		this.health = 20;
-		this.mana = 0;
-		deck = new Deck();
-		// not sure how to initialize the hand here
+		this.mana = 0;		
 		this.dead = false;
 		this.turn = false;		
-		avatar = new Avatar();
-		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
-		//not sure how to use this
+	
+	
 	}
 	
 	public Player(int health, int mana) {
 		super();
 		this.health = health;
 		this.mana = mana;
-		deck = new Deck();
-		// not sure how to initialize the hand here
 		this.dead = false;
 		this.turn = false;		
-		avatar = new Avatar();
-		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
-		//not sure how to use this
-	
+
 	}
 		
 	public int getHealth() {
@@ -61,21 +63,21 @@ public class Player {
 		this.mana = mana;
 	}
 	
-
+	//add Mana and check maximum
 	public void addMana(int addMana) {
 		int newMana = mana + addMana;
 		if(newMana > 9) {
 			this.mana = 9;
-		}
-		
+		}	
 	}
+	//delete Mana  (check minimum?)
 	public void loseMana(int loseMana) {
 		mana = mana - loseMana;
-	//minimum mana will be checked when playing cards?
+
 	}	
 	
 	
-	
+	//add Health and check maximum
 	public void addHealth(int addHealth) {
 		int newHealth = health + addHealth;
 		if (newHealth <= 20 && newHealth > 0 ) {
@@ -85,6 +87,7 @@ public class Player {
 			this.health = 20;
 		}
 	}	
+	//lose health and check if it's dead, health =0
 	public void loseHealth(int loseHealth) {
 		int newHealth = health - loseHealth;
 		if(newHealth <= 0) {
@@ -92,11 +95,15 @@ public class Player {
 			GameState.gameOver();
 		}
 	}	
+	
+	/*  below methods move to GameState/EndTurn?
 	public void checkDead() {
 		if (dead = true) {
 			GameState.gameOver();
 		}
 	}	
+	
+	
 	public void setTurn(){
 		if (turn){
 			turn = false;
@@ -105,5 +112,34 @@ public class Player {
 		}
 	}
 	
+	*/
+	
+	
+	public Avatar getAvatar() {
+		return avatar;
+	}
+	
+	public void setAvatar(Avatar a) {
+		this.avatar = a;
+		
+	}
+	
+	//create deck for player
+	public void assignDeck() {
+		this.deck.getDeck();
+		
+	}
+	
+	//create hand for player, along with first 3 cards in hand
+	public void seeHand() {
+		this.hand = new Hand(null);
+		this.hand.initialHand(out, deck);
+	}
+	
+	//draw card from hand, top card in deck is deleted
+		public void drawFromDeck() {
+			this.hand.drawCard(out, deck); 
+
+		}
 	
 }
