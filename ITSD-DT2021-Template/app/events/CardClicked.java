ab5e321e77a7b5fefbb18605ac234920622438d3
NoahS-Upton <2190985u@student.gla.ac.uk>
2021-06-21 18:46:50 +0100

    Add loops to check whether unit selected is spell or monster

--- ITSD-DT2021-Template/app/events/CardClicked.java
+++ ITSD-DT2021-Template/app/events/CardClicked.java
@@ -35,27 +35,30 @@ public class CardClicked implements EventProcessor{
 		tempHand=gameState.getPlayerOne().getHand();
 		tempSelect=tempHand.setSelectedCard(tempHand.getCardFromHand(handPosition));
 
-		
-		
 		//method for if no card is currently selected
 		if(gameState.getTurnOwner()==gameState.getPlayerOne && gameState.getPlayerOne().getHand().getClicked==false)
 		{
 		gameState.getPlayerOne().getHand().getCardFromHand(handPosition).setClicked(true);
 		gameState.getPlayerOne().getHand().setPlayingMode(true);
 		gameState.getPlayerOne().getHand().setSelectedCard(tempHand.getCardFromHand(handPosition));
-		//if tempSelect is type monster{}
-		ArrayList<Tile> display= gameState.getGameBoard().allSummonableTiles(gameState.getPlayerOne);
+		if (tempSelect.getBigCard().getAttack() > 0){
+			ArrayList<Tile> display= gameState.getGameBoard().allSummonableTiles(gameState.getPlayerOne);
 		for(Tile t: display) {
 			BasicCommands.DrawTile(out,t,2);
-			}//else if tempSelect is type spell{}
+			}
+		}else if (tempSelect.getBigCard().getAttack() < 0) {
+			ArrayList<Tile> display= gameState.getGameBoard().allSummonableTiles(gameState.getPlayerOne);
+			for(Tile t: display) {
+				BasicCommands.DrawTile(out,t,2);
+				}
 		}
-		
+		}
 		// switch to new card when one already selected
 		//else if{
 		//(gameState.getTurnOwner()==playerOne && gameState.getPlayerOne().getHand().getClicked==true)
 		//
 		//
-		// ArrayList<Tile> display= GameState.getGameBoard().friendlyMonsterTiles(gameState.getPlayerOne);
+		// ArrayList<Tile> display= gameState.getGameBoard().friendlyMonsterTiles(gameState.getPlayerOne);
 		//for(Tile t: display) {BasicCommands.DrawTile(out,t,2);}
         //}
 	}
