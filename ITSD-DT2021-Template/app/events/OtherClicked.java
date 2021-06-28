6f3dd978cf71080332acc57d697008accf400fde
2585743J <2585743J@student.gla.ac.uk>
2021-06-28 04:52:00 +0100

    De-select core code in place, still needs Hand method input

--- ITSD-DT2021-Template/app/events/OtherClicked.java
+++ ITSD-DT2021-Template/app/events/OtherClicked.java
@@ -1,9 +1,16 @@
 package events;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
+import commands.GeneralCommandSets;
 import structures.GameState;
+import structures.basic.Card;
+import structures.basic.Monster;
+import structures.basic.Tile;
 
 /**
  * Indicates that the user has clicked an object on the game canvas, in this case
@@ -21,10 +28,50 @@ public class OtherClicked implements EventProcessor{
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		
-		// Reset selected entities 
-		gameState.getBoard().setUnitSelected(null);
-		gameState.getTurnOwner().getHand().setPlayingMode(false);
-		gameState.getTurnOwner().getHand().setSelectedCard(null);
+		// >>> Deselect all
+		
+		// Card selected
+		if(gameState.getTurnOwner().getHand().isPlayingMode()) {
+			
+			Card cSelected = gameState.getTurnOwner().getHand().getSelectedCard();
+			
+			// Visual update while references intact	--- Need position info from Noah somehow
+			//BasicCommands.drawCard(out, cSelected, position, 0);
+			//GeneralCommandSets.threadSleep();
+			
+			// Update game variables
+			gameState.getTurnOwner().getHand().setSelectedCard(null);
+			//gameState.getTurnOwner().getHand().getSelectedCard().setClicked(false);
+			gameState.getTurnOwner().getHand().setPlayingMode(false);
+			
+		}
+		
+		// Unit selected
+		if(gameState.getBoard().getUnitSelected() != null) {
+			
+			Monster mSelected = gameState.getBoard().getUnitSelected();
+			
+			// Visual update while references intact
+			ArrayList <Tile> actionRange = gameState.getBoard().unitMovableTiles(mSelected.getPosition().getTilex(), mSelected.getPosition().getTiley(), mSelected.getMovesLeft());
+			ArrayList <Tile> attackRange = new ArrayList <Tile> (gameState.getBoard().unitAttackableTiles(mSelected.getPosition().getTilex(), mSelected.getPosition().getTiley(), mSelected.getAttackRange(), mSelected.getMovesLeft()));
+			actionRange.addAll(attackRange);
+			
+			for(Tile t : actionRange) {
+				BasicCommands.drawTile(out, t, 0);
+				GeneralCommandSets.threadSleep();
+			}
+			
+			// + Unit tile
+			BasicCommands.drawTile(out, gameState.getBoard().getTile(mSelected.getPosition().getTilex(), mSelected.getPosition().getTiley()), 0);
+			GeneralCommandSets.threadSleep();
+			
+			// Update game variables
+			gameState.getBoard().setUnitSelected(null);
+			mSelected.toggleSelect();
+			
+			System.out.println("Monster selected: " + mSelected.isSelected());
+			
+		}
 
 		/*
 		 * Will hold:
@@ -32,8 +79,6 @@ public class OtherClicked implements EventProcessor{
 		 * Deselect Card
 		 */
 		
-		// Deselect any Monsters that are selected
-		// Could put this in a method stored somewhere else, which can just be called wherever in program
 		
 	}
 
