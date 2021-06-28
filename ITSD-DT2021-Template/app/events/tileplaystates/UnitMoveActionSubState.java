7d447fdbfbd084d2f065620d38e9068968098e4b
2585743J <2585743J@student.gla.ac.uk>
2021-06-28 05:06:23 +0100

    Adjustments to support sequential related state creation, still rough

--- ITSD-DT2021-Template/app/events/tileplaystates/UnitMoveActionSubState.java
+++ ITSD-DT2021-Template/app/events/tileplaystates/UnitMoveActionSubState.java
@@ -6,6 +6,7 @@ import akka.actor.ActorRef;
 import commands.BasicCommands;
 import commands.GeneralCommandSets;
 import events.UnitMoving;
+import events.UnitStopped;
 import structures.GameState;
 import structures.basic.Monster;
 import structures.basic.Tile;
@@ -22,7 +23,10 @@ public class UnitMoveActionSubState implements GameplayStates {
 		unitMove(context); 
 		
 		// Call UnitMoving
-		UnitMoving rules = new UnitMoving(); 
+//		UnitMoving rules = new UnitMoving(); 
+		
+		// Call UnitStopped
+//		UnitStopped continued = new UnitStopped();
 		
 	}
 
@@ -37,12 +41,14 @@ public class UnitMoveActionSubState implements GameplayStates {
 		int tilex = context.getTilex(); 
 		int tiley = context.getTiley(); 
 
-		// Get Monster and movement range
+		// Get frequently used objects
 		Monster mSelected = gameState.getBoard().getUnitSelected();
 		ArrayList <Tile> mRange = gameState.getBoard().unitMovableTiles(mSelected.getPosition().getTilex(),mSelected.getPosition().getTiley(),mSelected.getMovesLeft());
-		
+		ArrayList <Tile> actRange = new ArrayList <Tile> (gameState.getBoard().unitAttackableTiles(mSelected.getPosition().getTilex(), mSelected.getPosition().getTiley(), mSelected.getAttackRange(), mSelected.getMovesLeft()));
+		actRange.addAll(mRange);
 		Tile current = gameState.getBoard().getTile(mSelected.getPosition().getTilex(),mSelected.getPosition().getTiley());
 		Tile target = gameState.getBoard().getTile(tilex, tiley);
+		System.out.println("Movement target tile is x: " + target.getTilex() + ", y: " + target.getTiley());
 
 		// If target tile is in movement range && monster can move, move there
 		System.out.println("MovesLeft: " + mSelected.getMovesLeft());
@@ -52,17 +58,16 @@ public class UnitMoveActionSubState implements GameplayStates {
 			System.out.println("Monster on cooldown: " + mSelected.getOnCooldown());
 			
 			// Deselect movement range --- implement specific visual path display later
-			ArrayList <Tile> actRange = new ArrayList <Tile> (context.getGameStateRef().getBoard().unitAttackableTiles(current.getTilex(), current.getTiley(), mSelected.getAttackRange(), mSelected.getMovesLeft()));
-			actRange.addAll(mRange);
 			GeneralCommandSets.drawBoardTiles(out, actRange, 0);
 			GeneralCommandSets.threadSleep();
 			// Redraw selected tile visual
 			BasicCommands.drawTile(out, current, 0);
 			GeneralCommandSets.threadSleep();GeneralCommandSets.threadSleep();
 
-			// Update Board and Tiles
+			// Update Tiles and Unit
 			current.removeUnit();
-			target.addUnit(mSelected);			
+			target.addUnit(mSelected);
+			mSelected.setPositionByTile(target);
 			BasicCommands.addPlayer1Notification(out, "Unit moving...", 4);
 			GeneralCommandSets.threadSleep();
 			
@@ -72,14 +77,15 @@ public class UnitMoveActionSubState implements GameplayStates {
 			GeneralCommandSets.threadSleep();
 			// Initiate move
 			BasicCommands.moveUnitToTile(out, mSelected, gameState.getBoard().getTile(tilex, tiley));
-			GeneralCommandSets.threadSleep();
-			// Re-idle
-			BasicCommands.playUnitAnimation(out, mSelected, UnitAnimationType.idle);
-			GeneralCommandSets.threadSleep();
+			GeneralCommandSets.threadSleep();		GeneralCommandSets.threadSleep();
 			
-			// Deselect after action finished
-			mSelected.toggleSelect();
-			context.deselectAllAfterActionPerformed();
+			/***	Condition here for combined substate executing, which requires selection is maintained	***/
+			if(!(context.getCombinedActive())) {
+				// Deselect after action finished *if* not in the middle of move-attack action
+				mSelected.toggleSelect();
+				context.deselectAllAfterActionPerformed();
+			}
+
 		}
 		
 		// Destination is not in movement range/unit cannot move
