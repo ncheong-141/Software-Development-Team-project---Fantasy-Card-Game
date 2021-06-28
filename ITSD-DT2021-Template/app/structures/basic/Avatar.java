beb29178a6cfbf0a7231c9fe2213e67a0a726476
Chiara Pascucci <2522106P@student.gla.ac.uk>
2021-06-28 13:56:40 +0100

    changes to avatar class: removing board as class variable and from constructor, still passing a board obj to the set owner method so that avatar starting position can be set automatically, but can move this logic to initialise if needed

--- ITSD-DT2021-Template/app/structures/basic/Avatar.java
+++ ITSD-DT2021-Template/app/structures/basic/Avatar.java
@@ -8,37 +8,30 @@ import java.util.ArrayList;
 //==========================================================================//
 
 public class Avatar extends Monster {
-	Board board;
+
 	
 	public Avatar() {}
-	
-	
-	public Avatar(Board b) {
-		super();
-		
-		this.board = b;
-	}
+
 	
 	//when the owner is set for an avatar, this method also takes care of assigning the avatar to the relevant 
 	//starting position, which is based on the type of owner
 	public void setOwner(Player p, Board b) {
 		this.owner = p;
-		this.board = b;
 		this.HP = p.getHealth();
-		this.setPositionByPlayer();
+		this.setPositionByPlayer(b);
 	}
 
 	//this method is used by the method above to check if the avatar's owner is a human or computer player
 	//depending on the type the the right starting position is assigned to the avatar
-	private void setPositionByPlayer () {
+	private void setPositionByPlayer (Board b) {
 		if (this.owner instanceof HumanPlayer) {
-			this.setPositionByTile(board.getTile(1,2));
-			board.getTile(1, 2).addUnit(this);
+			this.setPositionByTile(b.getTile(1,2));
+			b.getTile(1, 2).addUnit(this);
 		}
 		
 		else if (this.owner instanceof ComputerPlayer) {
-			this.setPositionByTile(board.getTile(7,2));
-			board.getTile(7,2).addUnit(this);
+			this.setPositionByTile(b.getTile(7,2));
+			b.getTile(7,2).addUnit(this);
 		}
 	}
 
