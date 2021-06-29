1bfe35ae13c7b13452a5b5a218ab7993003f3012
2585743J <2585743J@student.gla.ac.uk>
2021-06-29 21:08:30 +0100

    Summon demo adjustments

--- ITSD-DT2021-Template/app/demo/CommandDemo.java
+++ ITSD-DT2021-Template/app/demo/CommandDemo.java
@@ -620,6 +620,7 @@ public class CommandDemo {
 		// Set up friendly Unit to summon next to
 		Avatar humanAvatar = g.getHumanAvatar();
 		humanAvatar.setOwner(g.getPlayerOne(), gameBoard);
+		humanAvatar.toggleCooldown();
 		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
 
 		humanAvatar.setAttackValue(2);
