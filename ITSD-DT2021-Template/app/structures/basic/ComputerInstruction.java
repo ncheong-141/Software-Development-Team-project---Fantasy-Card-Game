4f518adc3c8ec277f6cb76e7050864112391423d
Chiara Pascucci <2522106P@student.gla.ac.uk>
2021-06-30 20:07:41 +0100

    adding new class to represent instructions generated from teh computer player to be processed and transmitted to the UI

---
+++ ITSD-DT2021-Template/app/structures/basic/ComputerInstruction.java
@@ -0,0 +1,18 @@
+package structures.basic;
+
+public class ComputerInstruction {
+	protected Card card;
+	protected Tile targetTile;
+	protected Monster actor;
+	
+	public ComputerInstruction(Card c, Tile t) {
+		this.card = c;
+		this.targetTile = t;
+		this.actor = null;
+	}
+	
+	public ComputerInstruction(Monster m, Tile t) {
+		this.actor = m;
+		this.targetTile = t;
+	}
+}
