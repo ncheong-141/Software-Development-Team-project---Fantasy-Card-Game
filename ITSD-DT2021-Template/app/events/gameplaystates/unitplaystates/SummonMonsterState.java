dba9597c16c6db763334cbb818881a413025a521
2585743J <2585743J@student.gla.ac.uk>
2021-07-03 03:11:07 +0100

    Added missing summon EffectAnimation

--- ITSD-DT2021-Template/app/events/gameplaystates/unitplaystates/SummonMonsterState.java
+++ ITSD-DT2021-Template/app/events/gameplaystates/unitplaystates/SummonMonsterState.java
@@ -9,6 +9,7 @@ import events.gameplaystates.GameplayContext;
 import events.gameplaystates.tileplaystates.ITilePlayStates;
 import structures.GameState;
 import structures.basic.Card;
+import structures.basic.EffectAnimation;
 import structures.basic.Monster;
 import structures.basic.Tile;
 import structures.basic.UnitAnimationType;
@@ -37,8 +38,11 @@ public class SummonMonsterState implements IUnitPlayStates {
 	public void execute(GameplayContext context) {
 		
 		System.out.println("In SummonMonsterSubState.");
-
-		summonMonster(context.getGameStateRef(), context.out, "dont have u_config file yet", context.getLoadedCard(), targetTile.getTilex(), targetTile.getTiley());
+		
+		// If targetTile is in summon range check
+		// Mana sufficient check
+		
+		summonMonster(context.getGameStateRef(), context.out, "dont have u_config file yet", context.getLoadedCard(), this.targetTile);
 		
 		/** Reset entity selection and board **/  
 		// Deselect after action finished *if* not in the middle of move-attack action
@@ -49,47 +53,9 @@ public class SummonMonsterState implements IUnitPlayStates {
 	}
 
 	
-	public void summonMonster(GameState gameState, ActorRef out, String u_configFile, Card statsRef, int tilex, int tiley) {
+	public void summonMonster(GameState gameState, ActorRef out, String u_configFile, Card statsRef, Tile summonTile) {
 		
-		// Summon the Monster (instantiate)
-		BasicCommands.addPlayer1Notification(out, "drawUnit", 2);
-		
-		// Need some code about retrieving StaticConfFiles matching card from Deck here
-		Monster summonedMonster = (Monster) BasicObjectBuilders.loadMonsterUnit(StaticConfFiles.u_fire_spitter,1,statsRef,Monster.class);		
-		summonedMonster.setPositionByTile(gameState.getBoard().getTile(tilex,tiley));
-		summonedMonster.setOwner(gameState.getTurnOwner());
-		GeneralCommandSets.threadSleep(); 
-		
-		// Keep before add Unit because drawTile doesn't work on tile underneath new monster for some reason
-		// De-highlight tiles
-		ArrayList <Tile> summonRange = gameState.getBoard().allSummonableTiles(gameState.getTurnOwner());
-		GeneralCommandSets.drawBoardTiles(out, summonRange, 0);
-		GeneralCommandSets.threadSleepLong();
-		
-		// Add unit to tile ON BOARD
-		BasicCommands.addPlayer1Notification(out, "Monster added to tile", 2);
-		gameState.getBoard().getTile(tilex,tiley).addUnit(summonedMonster);
-		GeneralCommandSets.threadSleep();
-		
-		// Drawing summoned monster with stats on the board
-		GeneralCommandSets.drawUnitWithStats(out, summonedMonster, gameState.getBoard().getTile(tilex, tiley));
-		GeneralCommandSets.threadSleep();
-		BasicCommands.playUnitAnimation(out, summonedMonster, UnitAnimationType.idle);
-		GeneralCommandSets.threadSleep();
-		
-		// Set monster statistics
-//		BasicCommands.setUnitHealth(out, summonedMonster, summonedMonster.getHP());
-//		GeneralCommandSets.threadSleep();
-//		BasicCommands.setUnitAttack(out, summonedMonster, summonedMonster.getAttackValue());
-//		GeneralCommandSets.threadSleep();
-		
-		// De-select card (visual only)
-		Card selectedCard = gameState.getTurnOwner().getHand().getSelectedCard();
-		// Need position in hand from Noah to redraw Card with no highlight
-//		BasicCommands.drawCard(out, selectedCard, position, 0);
-		
-
-		// >>> Mana costs --- leave out until mana cycle is implemented ingame
+		// Mana cost
 //		BasicCommands.addPlayer1Notification(out, "Player mana cost", 2);
 //		gameState.getTurnOwner().loseMana(statsRef.getManacost());
 //		
@@ -101,7 +67,45 @@ public class SummonMonsterState implements IUnitPlayStates {
 //		
 //		try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
 		
-		// >>> Delete card from Hand command --- later sprint
+		// Summon the Monster (instantiate)
+		BasicCommands.addPlayer1Notification(out, "drawUnit", 2);
+		
+		// Need some code about retrieving StaticConfFiles matching card from Deck here
+		Monster summonedMonster = (Monster) BasicObjectBuilders.loadMonsterUnit(StaticConfFiles.u_fire_spitter,1,statsRef,Monster.class);		
+		summonedMonster.setPositionByTile(summonTile);
+		summonedMonster.setOwner(gameState.getTurnOwner());
+		GeneralCommandSets.threadSleep(); 
+		
+		// Summon animation
+		EffectAnimation summonEf = BasicObjectBuilders.loadEffect(StaticConfFiles.f1_summon);
+		BasicCommands.playEffectAnimation(out, summonEf, summonTile);
+		
+		// Add unit to tile ON BOARD
+		BasicCommands.addPlayer1Notification(out, "Monster added to tile", 2);
+		summonTile.addUnit(summonedMonster);
+		GeneralCommandSets.threadSleep();
+		
+		// Drawing summoned monster with stats on the board
+		GeneralCommandSets.drawUnitWithStats(out, summonedMonster, summonTile);
+		GeneralCommandSets.threadSleep();
+		BasicCommands.playUnitAnimation(out, summonedMonster, UnitAnimationType.idle);
+		GeneralCommandSets.threadSleep();
+		
+		// Set monster statistics
+		BasicCommands.setUnitHealth(out, summonedMonster, summonedMonster.getHP());
+		GeneralCommandSets.threadSleep();
+		BasicCommands.setUnitAttack(out, summonedMonster, summonedMonster.getAttackValue());
+		GeneralCommandSets.threadSleep();
+		
+		// Delete card from hand
+//		Card selectedCard = context.getLoadedUnit();
+		// Need position in hand from Noah to redraw Card with no highlight
+//		BasicCommands.drawCard(out, selectedCard, intPosition, 0);
+//		GeneralCommandSets.threadSleep();
+//		BasicCommands.deleteCard(out, intPosition);
+//		GeneralCommandSets.threadSleep();
+		
+		
 	}
 	
 	
