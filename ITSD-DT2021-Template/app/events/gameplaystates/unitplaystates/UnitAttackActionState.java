a44655ccece5067d5ed36f7088278722e40dd268
2585743J <2585743J@student.gla.ac.uk>
2021-07-03 06:19:46 +0100

    Added supplementary code for attack state checks, +commentary on future needs/changes

--- ITSD-DT2021-Template/app/events/gameplaystates/unitplaystates/UnitAttackActionState.java
+++ ITSD-DT2021-Template/app/events/gameplaystates/unitplaystates/UnitAttackActionState.java
@@ -2,6 +2,7 @@ package events.gameplaystates.unitplaystates;
 
 import java.util.ArrayList;
 
+import ch.qos.logback.core.Context;
 import commands.BasicCommands;
 import commands.GeneralCommandSets;
 import events.gameplaystates.GameplayContext;
@@ -13,18 +14,28 @@ import structures.basic.UnitAnimationType;
 public class UnitAttackActionState implements IUnitPlayStates {
 
 	/*** State attributes ***/
-	private Tile currentTile; 
-	private Tile targetTile; 
+	private Tile 				currentTile; 
+	private Tile 				targetTile; 
+	
+	private Monster				attacker;
+	private Monster				defender;
+	
+	private ArrayList <Tile>	attackerAttackRange;
+	private ArrayList <Tile>	defenderCounterRange;
 	
 	/*** State constructor ***/
 	/* 
 	 * Changed constructor to input current and target tiles to decouple Unit states from TileClicked
-	 * Previously Unit states had tilex, tiley be used from context which were variables recieved from TileClicked. 
+	 * Previously Unit states had tilex, tiley be used from context which were variables received from TileClicked. 
 	 * Decoupling required to use unit States from the ComputerPlayer. */
 	
 	public UnitAttackActionState(Tile currentTile, Tile targetTile) {
 		this.currentTile = currentTile;
 		this.targetTile = targetTile; 
+		this.attacker = null;
+		this.defender = null;
+		this.attackerAttackRange = null;
+		this.defenderCounterRange = null;
 	}
 	
 	public void execute(GameplayContext context) {
@@ -32,57 +43,70 @@ public class UnitAttackActionState implements IUnitPlayStates {
 		System.out.println("In UnitAttackActionSubState.");
 		context.debugPrint();
 		
-		unitAttack(context);
+		// Gather attacker and defender
+		attacker = (Monster) context.getLoadedUnit();
+		defender = targetTile.getUnitOnTile();
 		
-		/***	Condition here for combined substate executing, which requires selection is maintained	***/
-		if(!(context.getCombinedActive())) {
+		// Gather ranges
+		// Build attacker's attackRange for checks --- formula needs to be changed to just adjacent attack tiles, as
+		// this state assumes units are next to each other before attack can occur
+		// For attacker: attackRange should reflect the attacks it has left + range
+		// For defender: attackRange should reflect an assumed ability to attack (range 1) since counter is not the same - generic adjacent range
+		ArrayList <Tile> temp = new ArrayList <Tile> (context.getGameStateRef().getBoard().unitAttackableTiles(currentTile.getTilex(), currentTile.getTiley(), attacker.getAttackRange(), attacker.getMovesLeft()));
+		attackerAttackRange = temp;
+		temp = new ArrayList <Tile> (context.getGameStateRef().getBoard().unitAttackableTiles(targetTile.getTilex(), targetTile.getTiley(), defender.getAttackRange(), defender.getMovesLeft()));
+		defenderCounterRange = temp;
+		
+		// Checks;
+		// Tile not in attack range
+		// Adjacent enemies with Provoke
+		if(tileInAttackRange()) {
+			unitAttack(context);
 			
-			/** Reset entity selection and board **/  
-			// Deselect after action finished *if* not in the middle of move-attack action
-			context.deselectAllAfterActionPerformed();
+			/***	Condition here for combined substate executing, which requires selection is maintained	***/
+			if(!(context.getCombinedActive())) {
+				
+				/** Reset entity selection and board **/  
+				// Deselect after action finished *if* not in the middle of move-attack action
+				context.deselectAllAfterActionPerformed();
+			
+				//  Reset board visual (highlighted tiles)
+				GeneralCommandSets.boardVisualReset(context.out, context.getGameStateRef());
+			}	
+			
+		} 
 		
-			//  Reset board visual (highlighted tiles)
-			GeneralCommandSets.boardVisualReset(context.out, context.getGameStateRef());
-		}	
+		else {
+			// Other verbose check messages to be added
+			
+			System.out.println("Enemy is not in attack range.");
+		}
+		
+
+
 	}
 	
 	private void unitAttack(GameplayContext context) {
-		
-		// Gather attacker and defender
-		Monster attacker = (Monster) context.getLoadedUnit();
-		Monster defender = targetTile.getUnitOnTile();
-		
-		// Retrieve frequently used Tile data
-		ArrayList <Tile> selectedAttackRange = new ArrayList <Tile> (context.getGameStateRef().getBoard().unitAttackableTiles(currentTile.getTilex(), currentTile.getTiley(), attacker.getAttackRange(), attacker.getMovesLeft()));
-	
-		// Check target is in attack range
-		if(!(selectedAttackRange.contains(targetTile))) {	
-			System.out.println("Enemy is not in range.");
-			return;
-		}
-		
+				
 		// Gather action range
 		ArrayList <Tile> actRange = new ArrayList <Tile> (context.getGameStateRef().getBoard().unitAttackableTiles(attacker.getPosition().getTilex(), attacker.getPosition().getTiley(), attacker.getAttackRange(), attacker.getMovesLeft()));
 		ArrayList <Tile> mRange = context.getGameStateRef().getBoard().unitMovableTiles(attacker.getPosition().getTilex(), attacker.getPosition().getTiley(), attacker.getMovesLeft());
 		actRange.addAll(mRange);
 		
-		// If attacker can attack
+		// If attacker can attack (true returned by Monster method)
 		if(attacker.attack()) {
 			System.out.println("Attack successful.");
-			boolean outcome = defender.defend(attacker.getAttackValue());
+			boolean survived = defender.defend(attacker.getAttackValue());
 			
 			// Deselect tiles
 			System.out.println("Started drawing Tiles");
 			//GeneralCommandSets.drawBoardTiles(context.out, actRange, 0);
-			int i = 0;
 			for (Tile t : actRange) {
 				// Leave defender tile highlighted
 				if(t == context.getClickedTile()) {
 					continue;
 				}
 				BasicCommands.drawTile(context.out, t, 0);
-				System.out.println("Drawn tile " + i);
-				i++;
 				try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
 			}
 			System.out.println("Finished drawing Tiles method");
@@ -95,19 +119,29 @@ public class UnitAttackActionState implements IUnitPlayStates {
 			BasicCommands.setUnitHealth(context.out, defender, defender.getHP());
 			try {Thread.sleep(30);} catch (InterruptedException e) {e.printStackTrace();}
 			
-			// Defender death check
-			// Counter attack here
+			// Defender death + counter attack check
+			if(!survived) {
+				System.out.println("Defender is dead.");
+				
+				// Play animation + sleep to let it happen
+				BasicCommands.playUnitAnimation(context.out, defender, UnitAnimationType.death);
+				try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				// Remove unit from front-end
+				BasicCommands.deleteUnit(context.out, defender);
+				try {Thread.sleep(30);} catch (InterruptedException e) {e.printStackTrace();}
+				
+				// Update internal Tile values
+				
+				// Destroy object?
+				
+			} else {
+				System.out.println("Counter-attack incoming...");
+				
+				// Counter attack here
+				
+			}
 			
-			// Re-idle (if condition for defender death later)
-			BasicCommands.playUnitAnimation(context.out,attacker,UnitAnimationType.idle);
-			BasicCommands.playUnitAnimation(context.out,defender,UnitAnimationType.idle);
-			GeneralCommandSets.threadSleep();
-			
-			// Update both unit tiles visuals
-			BasicCommands.drawTile(context.out, context.getGameStateRef().getBoard().getTile(attacker.getPosition().getTilex(), attacker.getPosition().getTiley()), 0);			
-			GeneralCommandSets.threadSleep();
-			BasicCommands.drawTile(context.out, targetTile, 0);
-			GeneralCommandSets.threadSleep();
 			
 		} 
 		// Unit is unable to attack for some reason - internal attack values/summon cooldown/etc.
@@ -115,6 +149,13 @@ public class UnitAttackActionState implements IUnitPlayStates {
 			System.out.println("Unit cannot attack.");
 		}
 		
+	}
+	
+	/*	Helper methods	*/
+	
+	private boolean tileInAttackRange() {
+		if(attackerAttackRange.contains(targetTile)) return true;
+		return false;
 	}
 	
 }
