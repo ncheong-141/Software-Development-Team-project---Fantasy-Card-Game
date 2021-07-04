2e72e9ac0244ef36a99d203177c1eea4d7c6add9
2585743J <2585743J@student.gla.ac.uk>
2021-07-04 16:24:09 +0100

    Improved comment readability

--- ITSD-DT2021-Template/app/structures/basic/abilities/AbilityToUnitLinkage.java
+++ ITSD-DT2021-Template/app/structures/basic/abilities/AbilityToUnitLinkage.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 
 import structures.basic.Avatar;
 import structures.basic.Monster;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
 
 /*
  * This class holds the information about the relationship between Units (Monsters and Spells) and what abilities they possess. 
@@ -23,23 +25,23 @@ public class AbilityToUnitLinkage {
 		/* Initiailising abilities with String name keys*/
 		// Can maybe grab this info from a file instead if dont want to hard code it in. 
 		
-		// For Abilities constructor (targets enemies?, Class type of target, EffectAnimation)
+		// For Abilities constructor (execution targets enemies?, Class type of target, EffectAnimation)
 		
-		/*** Spells ***/
-		UnitAbility.put("Truestrike", 		constructArrayListAbility(new A_Truestrike(true,Monster.class, null))); 			// Truestrike 
-		UnitAbility.put("Sundrop Elixir", 	constructArrayListAbility(new A_SundropElixir(false, Monster.class, null)));		// Sundrop Elixir
-		UnitAbility.put("Staff of Y'Kir", 	constructArrayListAbility(new A_StaffofYkir(false, Avatar.class, null)));			// Staff of Y'kir
-		UnitAbility.put("Entropic Decay", 	constructArrayListAbility(new A_EntropicDecay(true, Monster.class, null)));			// Entropic Decay
-
-		
-		/*** Units ***/ 
-		UnitAbility.put("Windshrike", 		constructArrayListAbility(new A_U_DrawCardOnUnitDeath(false, Monster.class, null))				// Windshrike
-											/*, + another ability*/);																
-		UnitAbility.put("Blaze Hound", 		constructArrayListAbility(new A_U_PlayersDrawCardOnUnitSummon(false, Monster.class, null)));	// Blaze Hound
-		UnitAbility.put("Azurite Lion", 	constructArrayListAbility(new A_U_DoubleAttacker(false, Monster.class, null)));					// Azurite Lion
-		UnitAbility.put("Serpenti", 		constructArrayListAbility(new A_U_DoubleAttacker(false, Monster.class, null)));					// Serpenti
-		UnitAbility.put("Fire Spitter", 	constructArrayListAbility(new A_U_RangedAttacker(false, Monster.class, null)));					// Fire Spitter
-		UnitAbility.put("Pyromancer", 		constructArrayListAbility(new A_U_RangedAttacker(false, Monster.class, null)));					// Pyromancer
+						/*** Spells ***/
+	/*Truestrike*/		UnitAbility.put("Truestrike", 		constructArrayListAbility(new A_Truestrike(true,Monster.class, null))); 	
+	/*Sundrop Elixir*/	UnitAbility.put("Sundrop Elixir", 	constructArrayListAbility(new A_SundropElixir(false, Monster.class, null)));
+	/*Staff of Y'Kir*/	UnitAbility.put("Staff of Y'Kir", 	constructArrayListAbility(new A_StaffofYkir(false, Avatar.class, null)));	
+	/*Entropic Decay*/	UnitAbility.put("Entropic Decay", 	constructArrayListAbility(new A_EntropicDecay(true, Monster.class, null)));	
+			
+					
+						/*** Units ***/ 
+	/*Windshrike*/		UnitAbility.put("Windshrike", 		constructArrayListAbility(new A_U_DrawCardOnUnitDeath(false, Monster.class, null))	
+															/*, + another ability*/);																
+	/*Blaze Hound*/		UnitAbility.put("Blaze Hound", 		constructArrayListAbility(new A_U_PlayersDrawCardOnUnitSummon(false, Monster.class, null)));
+	/*Azurite Lion*/	UnitAbility.put("Azurite Lion", 	constructArrayListAbility(new A_U_DoubleAttacker(false, Monster.class, null)));	
+	/*Serpenti*/		UnitAbility.put("Serpenti", 		constructArrayListAbility(new A_U_DoubleAttacker(false, Monster.class, null)));	
+	/*Fire Spitter*/	UnitAbility.put("Fire Spitter", 	constructArrayListAbility(new A_U_RangedAttacker(false, Monster.class, (BasicObjectBuilders.loadEffect(StaticConfFiles.f1_projectiles)))));	
+	/*Pyromancer*/		UnitAbility.put("Pyromancer", 		constructArrayListAbility(new A_U_RangedAttacker(false, Monster.class, (BasicObjectBuilders.loadEffect(StaticConfFiles.f1_projectiles)))));	
 		
 	}
 	
