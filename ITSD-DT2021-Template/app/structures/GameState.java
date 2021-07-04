26c67f6e60ca744bc545fee75a7c70be81ae4db9
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-07-04 14:42:18 +0200

    Commenting and cleaning up code

--- ITSD-DT2021-Template/app/structures/GameState.java
+++ ITSD-DT2021-Template/app/structures/GameState.java
@@ -1,6 +1,6 @@
 package structures;
 
-import events.tileplaystates.GameplayStates;
+import events.gameplaystates.tileplaystates.ITilePlayStates;
 import structures.basic.Avatar;
 import structures.basic.Board;
 import structures.basic.ComputerPlayer;
@@ -19,49 +19,50 @@ import utils.StaticConfFiles;
  */
 public class GameState {
 
-	private Board gameBoard;
-	private HumanPlayer playerOne;
-	private ComputerPlayer playerTwo;
-	private Avatar humanAvatar;
-	private Avatar computerAvatar;
-	private int turnCount;
-	private static boolean playerDead;
-	private Player turnOwner;
+	/** GameState attributes **/
+	private Board 			gameBoard;			// Board object which holds all Unit positions aswell as contains operations to find specific tiles sets. 
+	private HumanPlayer 	playerOne;			// Player one, the human player which holds all data for the player such as Hand and Deck for holding current cards. Also holds the control flow for drawing Cards from a Deck etc 
+	private ComputerPlayer 	playerTwo;			// Player two, computer player which holds the same as the above + AI logic for ranking combinations of instructions and actioning them. 
+	private Avatar 			humanAvatar;		// Do we need avatars in gameState? can it not just be in Board? 
+	private Avatar 			computerAvatar;
+	private int			 	turnCount;			// Tracker variable for the current number of turns 
+	private static boolean 	playerDead;			// Boolean variable which is set when either avatar is defeated
+	private Player 			turnOwner;			// The current turn owner of the game, refered to for certain checks such as having permission to click (the human player should not be able to select anything during the AI turn) 
 
+	
+	/** Constructor **/
 	public GameState() {
-		
+	
+		/* Set attributes */ 
 		turnCount = 0;
 		playerDead = false;
 		turnOwner = playerOne;
-		
-		
-		//decks instantiation 
+				
 
+		// Deck instantiations 
 		Deck deckPlayerOne = new Deck(); 
 		deckPlayerOne.deckOne();
-		
-		for (int i = 0; i < deckPlayerOne.getDeck().size(); i++) {
-			System.out.println(deckPlayerOne.getDeck().get(i).getCardname());
-		}
-		//playerOne.setDeck(deckPlayerOne);
+		// playerOne.setDeck(deckPlayerOne);
 				
 		Deck deckPlayerTwo = new Deck();
 		deckPlayerTwo.deckTwo();
 		//playerTwo.setDeck(deckPlayerTwo);
 		
+		// Instantiate players 								// GOING TO NEED TO REMOVE DECKS SOON 
 		playerOne = new HumanPlayer(deckPlayerOne);
 		playerTwo = new ComputerPlayer(deckPlayerTwo);
-
 		
+
+		// Board instantiation (Change Avatars to be instantiated in initialise methods and remove Avatar from gameState) 
 		gameBoard = new Board();
-		humanAvatar =  (Avatar) BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 0, Avatar.class);
+		humanAvatar = BasicObjectBuilders.loadAvatar(StaticConfFiles.humanAvatar, 0, Avatar.class);
 		humanAvatar.setOwner(playerOne, gameBoard);//assigning avatar to player and board - this could be done within player's class
 		
-		
-		computerAvatar = (Avatar) BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 1, Avatar.class);
+		computerAvatar = BasicObjectBuilders.loadAvatar(StaticConfFiles.aiAvatar, 1, Avatar.class);
 		computerAvatar.setOwner(playerTwo, gameBoard);
-		
 	}
+	
+	/** GameState methods: Getters and setters + some helper methods**/
 	
 	public int getTurnCount() {
 		return turnCount;
@@ -71,6 +72,7 @@ public class GameState {
 		this.turnCount = turnCount;
 	}
 
+	
 	public boolean isPlayerDead() {
 		return playerDead;
 	}
@@ -79,6 +81,7 @@ public class GameState {
 		this.playerDead = playerDead;
 	}
 
+	
 	public Player getTurnOwner() {
 		return turnOwner;
 	}
@@ -87,9 +90,21 @@ public class GameState {
 		this.turnOwner = turnOwner;
 	}
 
+	public void turnChange() {
+		if (turnOwner == playerOne) {
+			turnOwner = playerTwo;
+		}
+		
+		else turnOwner = playerOne;
+		
+		turnCount++;
+	}
+	
+	
 	public Board getGameBoard() {
 		return gameBoard;
 	}
+	
 
 	public HumanPlayer getPlayerOne() {
 		return playerOne;
@@ -99,6 +114,7 @@ public class GameState {
 		return playerTwo;
 	}
 
+	// Potentially remove
 	public Avatar getHumanAvatar() {
 		return humanAvatar;
 	}
@@ -107,6 +123,7 @@ public class GameState {
 		return computerAvatar;
 	}
 
+	// Is this necessary? 
 	public void setPlayers(HumanPlayer h, ComputerPlayer c) {
 		playerOne = h;
 		playerTwo = c;
@@ -118,21 +135,11 @@ public class GameState {
 		// call method to finish game
 	}
 	
-	public void turnChange() {
-		if (turnOwner == playerOne) {
-			turnOwner = playerTwo;
-		}
-		
-		else turnOwner = playerOne;
-		
-		turnCount++;
-	}
-
-	
-	/* Getters*/ 
+	// Errr we have two of these!
 	public Board getBoard() {
 		return gameBoard; 
 	}
+	
 	
 	/** Entity selection helper methods **/
 	
@@ -145,8 +152,9 @@ public class GameState {
 		}
 
 		// If there is a card selected in turn owner hand
-		if(this.getTurnOwner().getHand().getSelectedCard() != null) {
+		if(this.getTurnOwner().getHand().isPlayingMode()) {
 			this.getTurnOwner().getHand().setSelectedCard(null);
+			this.getTurnOwner().getHand().setPlayingMode(false);
 		}
 		
 	}
