f54113b894c4cd1e250bde473316d81093eab569
2585743J <2585743J@student.gla.ac.uk>
2021-07-04 03:28:40 +0100

    Added ranged and double attacker ability classes

---
+++ ITSD-DT2021-Template/app/structures/basic/abilities/A_U_DoubleAttacker.java
@@ -0,0 +1,63 @@
+
+package structures.basic.abilities;
+import structures.GameState;
+import structures.basic.Monster;
+
+public class A_U_DoubleAttacker implements Ability {
+	
+		// Ability attributes 
+		private boolean enemyTarget; 
+		private Class<? extends Monster> targetType; 
+		private Call_IDs callID; 
+		
+		// Constructors
+		public A_U_DoubleAttacker(boolean enemyTarget, Class<? extends Monster> targetType) {
+			this.enemyTarget = enemyTarget;
+			this.targetType = targetType; 
+		
+			this.callID = Call_IDs.construction;
+		}
+		
+		public A_U_DoubleAttacker() {
+			this.enemyTarget = false;
+			this.targetType = null; 
+			
+			this.callID = Call_IDs.construction;
+		}
+		
+		/* Class methods */
+		
+		// ABILITY IMPLEMENTATION
+		// ================================================================================
+		// Monster has the ability to attack twice
+		// Ignore GameState input since Ability deals with Unit internal values
+		public boolean execute(Monster targetMonster, GameState gameState) {
+			return execute(targetMonster); 
+		}
+		
+		// Alternative method signature that reflects actual behaviour
+		public boolean execute(Monster targetMonster) {
+
+			targetMonster.setAttacksMax(2);
+			return true; 
+		}
+		// ================================================================================
+		
+		
+		// Getters to communicate target and call chronology information
+		public boolean targetEnemy() {
+			return enemyTarget; 
+		}
+		
+		public Class<? extends Monster> getTargetType() {
+			return targetType; 
+		}
+		
+		public Call_IDs getCallID() { 
+			return callID;
+		}
+
+}
+
+	
+
---
+++ ITSD-DT2021-Template/app/structures/basic/abilities/A_U_RangedAttacker.java
@@ -0,0 +1,63 @@
+
+package structures.basic.abilities;
+import structures.GameState;
+import structures.basic.Monster;
+
+public class A_U_RangedAttacker implements Ability {
+	
+		// Ability attributes 
+		private boolean enemyTarget; 
+		private Class<? extends Monster> targetType; 
+		private Call_IDs callID; 
+		
+		// Constructors
+		public A_U_RangedAttacker(boolean enemyTarget, Class<? extends Monster> targetType) {
+			this.enemyTarget = enemyTarget;
+			this.targetType = targetType; 
+		
+			this.callID = Call_IDs.construction;
+		}
+		
+		public A_U_RangedAttacker() {
+			this.enemyTarget = false;
+			this.targetType = null; 
+			
+			this.callID = Call_IDs.construction;
+		}
+		
+		/* Class methods */
+		
+		// ABILITY IMPLEMENTATION
+		// ================================================================================
+		// Monster can attack from anywhere on the Board
+		// Ignore GameState input since Ability deals with Unit internal values
+		public boolean execute(Monster targetMonster, GameState gameState) {
+			return execute(targetMonster); 
+		}
+		
+		// Alternative method signature that reflects actual behaviour
+		public boolean execute(Monster targetMonster) {
+
+			targetMonster.setAttackRange(50);
+			return true; 
+		}
+		// ================================================================================
+		
+		
+		// Getters to communicate target and call chronology information
+		public boolean targetEnemy() {
+			return enemyTarget; 
+		}
+		
+		public Class<? extends Monster> getTargetType() {
+			return targetType; 
+		}
+		
+		public Call_IDs getCallID() { 
+			return callID;
+		}
+
+}
+
+	
+
