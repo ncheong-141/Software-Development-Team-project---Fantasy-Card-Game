99978302de09718b002001c31a9239c27d3e90ac
Chiara Pascucci <2522106P@student.gla.ac.uk>
2021-07-06 10:09:36 +0100

    adding new classes to contain computer logic more neatly

---
+++ ITSD-DT2021-Template/app/structures/basic/ComputerLogic/ComputerMoveMonsterLogic.java
@@ -0,0 +1,5 @@
+package structures.basic.ComputerLogic;
+
+public class ComputerMoveMonsterLogic {
+
+}
---
+++ ITSD-DT2021-Template/app/structures/basic/ComputerLogic/ComputerPlayCardsLogic.java
@@ -0,0 +1,15 @@
+package structures.basic.ComputerLogic;
+
+import structures.basic.Board;
+import structures.basic.Hand;
+
+public class ComputerPlayCardsLogic {
+	private Hand hand;
+	private Board board;
+	public ComputerPlayCardsLogic(Hand hand, Board board) {
+		this.hand = hand;
+		this.board = board;
+	}
+	
+	
+}
