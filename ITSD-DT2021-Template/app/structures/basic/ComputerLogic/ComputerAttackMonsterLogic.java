cea62a93091d59a2866cbcbc5b159faa8651acad
Peter2015 <2502089c@student.gla.ac.uk>
2021-07-07 01:56:23 +0100

    simplify Computer Attack logic

--- ITSD-DT2021-Template/app/structures/basic/ComputerLogic/ComputerAttackMonsterLogic.java
+++ ITSD-DT2021-Template/app/structures/basic/ComputerLogic/ComputerAttackMonsterLogic.java
@@ -1,9 +1,12 @@
 package structures.basic.ComputerLogic;
 import java.util.ArrayList;
 
+import events.gameplaystates.*;
+import events.gameplaystates.unitplaystates.*;
 import structures.GameState;
 import structures.basic.*;
 public class ComputerAttackMonsterLogic {
+	
 	private Board gameBoard;
 	private ComputerPlayer playerTwo;
 	private GameState gameState;
@@ -11,15 +14,13 @@ public class ComputerAttackMonsterLogic {
 	private ArrayList<Tile> targetInRange;  //a list of attackable enemy for each attacker
 	private Tile target; //attack target
 	private Tile attacker; 
+//	private UnitAttackActionState attackStart;
 
 	public ComputerAttackMonsterLogic(ComputerPlayer p) {
 		this.playerTwo = p;
 		this.gameBoard = p.getGameBoard();
 	}
 	
-	//////////////Yufen Attack Method/////
-
-	//if wrap all selecting target methods in another class, this is in ComputerPlayer or AI execution class?
 	public void smartAttack(){
 		    
 		attackerTiles = findAttacker(); //return a list of monster that can perform attack in this turn
@@ -31,45 +32,43 @@ public class ComputerAttackMonsterLogic {
 				continue;
 			}else{    //if there is good target, perform attack 
 				target = selectAttackTarget(attacker);
-			//a.unitAttack(attacker, target);
-	      //events/gameplaystates/unitplaystates/AIUnitStateController
-		        }
-		    }
+				
+				//attack action from here
+				if(attacker.getUnitOnTile().attack()) { 
+					System.out.println("Attack successful.");
+				}
+			}        
 		}
+	}
 	//////////////////////////Yufen Attack Logic/////////////////////////
 	/*
-	new class(findingBestTarget?) function
 
 	loop through all attackable monsters can perform attack:
 
 	for each attacker:
 	get attackable tile range for this attacker 
-	(will get new list of enemy every loop, without those were killed by previous attacker), would this solve the concern that you mentioned?
-	call target selecting method, if nothing return, for this attacker. choose not to attack
+	(will get new list of enemy every loop(attacker monster), without those were killed by previous attacker)
+	call target selecting method,
+			Target Selection Priority:
+				1.try kill the one with highest attackValue, if none
+				2.try kill the one with highest attackLeft, if none
+				3.try kill the one with lowest HP, if can't kill any
+				4.otherwise, it's not worth attack
 
-	Target Priority:
-	1.try kill the one with highest attackValue, if none
-	2.try kill the one with highest attackLeft, if none
-	3.try kill the one with lowest HP, if can't keill any
-	4.check if attacker will be killed easily in the next turn after enemy counter attack 
-	5.otherwise, it's not worth attack
-
+	 if not target return after selection, for this attacker. choose not to attack
+	 go on to next actionable attacker
 	*/
 
 
 
-	//<helper>
+	//<helper> target selection process
 	public Tile selectAttackTarget(Tile t){
 		    
-			
 		if( getHightAttackValue(t) != null){
 			return target;
 		}else if(getHightAttackLeft(t) != null){
 			return target;   
 		}else if ( getLowHP(t) != null){  
-			return target;
-		}
-		else if (harmless(t) != null){
 			return target;
 		} else {
 			return null;
@@ -77,8 +76,8 @@ public class ComputerAttackMonsterLogic {
 	}
 
 
-	//<helper>
-	public Tile getHightAttackValue(Tile t){
+	//<helper> select target in range with high damage, and can be killed
+	private Tile getHightAttackValue(Tile t){
 		    
 	//from a list of t attackable tiles range, find the tile hold enemy with highest attack value, and check if it can be killed after attack.
 		Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
@@ -97,8 +96,8 @@ public class ComputerAttackMonsterLogic {
 		} 
 	}
 
-	//<helper>
-	public Tile getHightAttackLeft(Tile t){
+	//<helper> select target in range with more times to attack, and can be killed
+	private Tile getHightAttackLeft(Tile t){
 		    
 		    //from a list of t attackable tiles range, find the tile hold enemy with highest attack times left, and check if it can be killed after attack.
 		Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
@@ -117,47 +116,47 @@ public class ComputerAttackMonsterLogic {
 		} 
 	}
 
-	//<helper>
-		public Tile getLowHP(Tile t){
+	//<helper> select any target in range with lowest HP, can be killed
+	private Tile getLowHP(Tile t){
 		    
-		    //from a list of t attackable tiles range, find the tile hold enemy with lowest HP left, and check if it can be killed after attack.
-		    Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
+		//from a list of t attackable tiles range, find the tile hold enemy with lowest HP left, and check if it can be killed after attack.
+		Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
 		
-		    //loop through targetInRange, find the enemy with highest attack times left
-		    for (int i = 1; i < targetInRange.size(); i++) {  
-		        if(targetInRange.get(i).getUnitOnTile().getHP() > chosenE.getUnitOnTile().getHP()) {  
-		            chosenE = targetInRange.get(i);  
-		        }	
-		    }
-		    // check if attacker can kill this one, avoid counter attack
-		    if ((chosenE.getUnitOnTile().getHP() - t.getUnitOnTile().getHP())<=0 ){  
-		        return chosenE;
-		    }else { 
-		        return null;
-		    } 
+		//loop through targetInRange, find the enemy with highest attack times left
+		for (int i = 1; i < targetInRange.size(); i++) {  
+			if(targetInRange.get(i).getUnitOnTile().getHP() > chosenE.getUnitOnTile().getHP()) {  
+				chosenE = targetInRange.get(i);  
+			}	
 		}
+		// check if attacker can kill this one, avoid counter attack
+		if ((chosenE.getUnitOnTile().getHP() - t.getUnitOnTile().getHP())<=0 ){  
+			return chosenE;
+		}else { 
+			return null; 
+		}
+	}
 
-	//<helper>
-		public Tile harmless(Tile t){
-		    //if couldn't find any enemy to kill, lower down the counter attack damage
-		    //from a list of t attackable tiles range, find the tile hold enemy with lowest attackValue(damage value), and make sure if won't be killed after counter attack
-		    Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
-		
-		    //loop through targetInRange, find the enemy with least attackValue
-		    for (int i = 1; i < targetInRange.size(); i++) {  
-		        if(targetInRange.get(i).getUnitOnTile().getAttackValue() < chosenE.getUnitOnTile().getAttackValue()) {  
-		            chosenE = targetInRange.get(i);  
-		        }	
-		    }
-		    // check if attacker will be killed easily in the next turn after enemy counter attack 
-		    // having remain HP >=2,somewhat can avoid being first target for HumanPlayer in the next turn
-		    // if set HP bar > 0, any HumanPlayer can attack in the next turn
-		    if ((t.getUnitOnTile().getHP() - chosenE.getUnitOnTile().getAttackValue()) >= 2 ){  
-		        return chosenE;
-		    }else { 
-		        return null;
-		    } 
-		}
+//	//<helper> this increase more work, back up for counter attack
+//		public Tile harmless(Tile t){
+//		    //if couldn't find any enemy to kill, lower down the counter attack damage
+//		    //from a list of t attackable tiles range, find the tile hold enemy with lowest attackValue(damage value), and make sure if won't be killed after counter attack
+//		    Tile chosenE = targetInRange.get(0);    //set compared chosenE as the first tile in targetInRange
+//		
+//		    //loop through targetInRange, find the enemy with least attackValue
+//		    for (int i = 1; i < targetInRange.size(); i++) {  
+//		        if(targetInRange.get(i).getUnitOnTile().getAttackValue() < chosenE.getUnitOnTile().getAttackValue()) {  
+//		            chosenE = targetInRange.get(i);  
+//		        }	
+//		    }
+//		    // check if attacker will be killed easily in the next turn after enemy counter attack 
+//		    // having remain HP >=2,somewhat can avoid being first target for HumanPlayer in the next turn
+//		    // if set HP bar > 0, any HumanPlayer can attack in the next turn
+//		    if ((t.getUnitOnTile().getHP() - chosenE.getUnitOnTile().getAttackValue()) >= 2 ){  
+//		        return chosenE;
+//		    }else { 
+//		        return null;
+//		    } 
+//		}
 
 
 	//<helper>
