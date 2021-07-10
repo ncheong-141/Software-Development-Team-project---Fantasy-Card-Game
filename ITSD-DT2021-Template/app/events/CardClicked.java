e7d2727a93e3c66ff53655b472f7dfcfcccb557b
NoahS-Upton <2190985u@student.gla.ac.uk>
2021-07-10 13:47:50 +0100

    changes to cardClicked when clearing all selected items

--- ITSD-DT2021-Template/app/events/CardClicked.java
+++ ITSD-DT2021-Template/app/events/CardClicked.java
@@ -34,23 +34,11 @@ public class CardClicked implements EventProcessor{
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		// Reset entity selection and board  
-		// Only reset the board if there is a unit selected (i.e. in display mode) 
-		if (gameState.getBoard().getUnitSelected() != null) {
-			// Reset board visual (highlighted tiles)
-			GeneralCommandSets.boardVisualReset(out, gameState);
-		}
-		// Deselect all entities (Card or Unit if selected) 
-		gameState.deselectAllEntities();
+		 // Reset entity selection and board
+        GeneralCommandSets.boardVisualReset(out, gameState);
+        gameState.deselectAllEntities();
 		
 		int handPosition = message.get("position").asInt();//gets position in hand of clicked card
-		
-		//checks if a card had previously been selected, if so it removes any traces of this
-		if(gameState.getTurnOwner().getHand().getSelectedCard()!=null){
-			Hand tempHand= gameState.getTurnOwner().getHand();
-			tempHand= gameState.getTurnOwner().getHand();
-			gameState.getTurnOwner().getHand().setSelectedCard(null);
-		}
 		
 		//creates a placeholder for the clicked card
 			Card clickedCard = gameState.getTurnOwner().getHand().getCardFromHand(handPosition);
@@ -59,25 +47,40 @@ public class CardClicked implements EventProcessor{
 			gameState.getTurnOwner().getHand().setSelectedCard(gameState.getTurnOwner().getHand().getCardFromHand(handPosition));
 			gameState.getTurnOwner().getHand().setSelCarPos(handPosition);
 			
+			// Reset entity selection and board
+	        GeneralCommandSets.boardVisualReset(out, gameState);
+	        gameState.deselectAllEntities();
+
+
 		//checks that the clicked card is a monster card using its attack value
 			// Check if the card has an ability that affects before summoning
-			if(clickedCard.hasAbility()) {
+
+			// Boolean switch to check if the ability is applicable 
+			boolean outputted = false; 
+
+			if(clickedCard.hasAbility()) {			
 				for(Ability a: clickedCard.getAbilityList()) {
-					
+
 					if (a.getCallID() == Call_IDs.onCardClicked) {			
 						// Execute it (null for no target monster)
 						a.execute(null, gameState); 
+						
+						System.out.println(a);
 						// Draw the respective tiles (any ability like this will only affect tiles really unless its like, "if you have this card in your had then get 2 HP per turn but that would be weird"/
 						GeneralCommandSets.drawBoardTiles(out, gameState.getTileAdjustedRangeContainer(), 2);
+						outputted = true; 
+						break; 
 					}
 				}
+				
 			}
-			else {
+			if (!outputted) {
 				// Else, draw the summonable tiles as normal
 				ArrayList<Tile> display= gameState.getBoard().allSummonableTiles(gameState.getTurnOwner());	
 				GeneralCommandSets.drawBoardTiles(out, display, 2);	
 			}
-		
+			
+
 		//a loop which checks that a card is a spell, then displays playable tiles depending on spell target
 		if (clickedCard.getBigCard().getAttack() < 0) {
 
