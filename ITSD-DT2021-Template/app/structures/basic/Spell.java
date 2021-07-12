c75f4bab9c9be8cf331c1bfe85e89c1c632ce2d4
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-07-12 04:39:08 +0200

    Further formatting.

--- ITSD-DT2021-Template/app/structures/basic/Spell.java
+++ ITSD-DT2021-Template/app/structures/basic/Spell.java
@@ -13,14 +13,13 @@ public class Spell extends Card {
 	protected static ObjectMapper mapper = new ObjectMapper(); // Jackson Java Object Serializer, is used to read java objects from a file
 	
 	
-	/* Sub class attributes */
-	private String spellName; 
-	private String abilityDescription; 
-	private Ability spellAbility; 
-	private Class<? extends Monster> targetMonster; 
+	/** Sub class attributes **/
+	private String spellName; 							// Name of spell
+	private Ability spellAbility; 						// Spell uses an ability, likewise to Monster, to enact its effects.
+	private Class<? extends Monster> targetMonster; 	// Specified target type in .class form
 	
 	
-	/* Constructor */
+	/** Constructor **/
 	public Spell(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard) {
 		
 		// Super class constructor (Unit) 
@@ -30,10 +29,10 @@ public class Spell extends Card {
 	// Required default constructor for jackson object instantiation
 	public Spell() {
 		super(); 
-		System.out.println("Im from the Spell constructor");
 	}
 
-	/* Class methods */
+	
+	/** Class methods **/
 	
 	public Ability getAbility() {
 		return spellAbility; 
